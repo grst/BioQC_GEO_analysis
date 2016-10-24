5ad6c7f447e468af3e577ef0b926895042d16e45
Gregor Sturm <mail@gregor-sturm.de>
2016-10-24 15:51:09 +0200

    Add method to collapse signatures

--- lib.R
+++ lib.R
@@ -48,7 +48,22 @@ bioqcHeatmap = function(bioqc_res, title) {
 }
 
 
-#' Collapse multiple signatures that belong to the same tissue into one. 
-collapseSignatures = function(table, sig_list, method=max) {
-  
+#' Collapse multiple signatures that belong to the same tissue into one.
+#' 
+#' BioQC comes with multiple signatures per tissue e.g. Liver, Liver_fetal
+#' Liver_NGS, ...
+#' The annotation in the GEO is more coarse than that, only stating 'liver'. 
+#' This function therefore collapses multiple signatures into one by taking the
+#' maximum score of the selected signatures. 
+#' 
+#' @param table
+#' @param sig.list list of the signature names corresponding to the rownames of the table
+#' @param new.name new row name for the collapsed rows. 
+#' @param method function to be applied to each column. Defaults to max.  
+collapseSignatures = function(table, sig.list, new.name, method=max) {
+    row.collapsed = apply(table[sig.list, ], 2, method)
+    row.collapsed.df = data.frame(t(unlist(row.collapsed)))
+    rownames(row.collapsed.df) = c(new.name)
+    table.collapsed = rbind(table, row.collapsed.df)
+    return(table.collapsed[-which(rownames(table.collapsed) %in% sig.list), ,drop=FALSE])
 }
