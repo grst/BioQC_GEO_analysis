ab1881df21b1532c9d633ed46e69267713e7ea40
Gregor Sturm <mail@gregor-sturm.de>
2016-10-25 09:18:07 +0200

    add tissue signatures for collapsing sigs

--- scripts/run_bioqc.R
+++ scripts/run_bioqc.R
@@ -2,20 +2,26 @@
 
 ########
 # USAGE:
-#   Rscript run_bioqc.R <eset.Rdata> <output_file.tab>
-# where eset.Rdata contains a biobase ExpressionSet with variable
-# name eset. 
+#   Rscript run_bioqc.R <esetName.Rdata> <outputPath>
+# where esetName.Rdata contains a biobase ExpressionSet with variable
+# name eset. outputPath will be appended with <esetName_bioqc_res.tab>
 #
 # The script applies BioQC to the ExpressionSet and stores the
 # raw p-values as a data table.
 ########
 
+library(tools)
+
 args = commandArgs(trailingOnly = TRUE)
 esetFile = args[1]
-outputFile = args[2]
+outputPath = args[2]
+geo_id = file_path_sans_ext(basename(esetFile))
+print(geo_id)
+
+outFile = file.path(outPath, paste(geoID, "_bioqc_res.tab", sep=""))
 
 #esetFile = "/homebasel/biocomp/sturmg/projects/GEO_BioQC/GDS_GPL570/GDS4074.Rdata"
-#outputFile = "/homebasel/biocomp/sturmg/projects/GEO_BioQC/BioQC_GEO_analysis/plots/GDS4074"
+#outFile = "/homebasel/biocomp/sturmg/projects/GEO_BioQC/BioQC_GEO_analysis/plots/GDS4074"
 
 library(BioQC)
 
@@ -27,4 +33,4 @@ load(esetFile)
 bioqcRes = wmwTest(eset, gmt, valType="p.greater", col="Gene symbol")
 
 #save result to table
-write.table(bioqcRes, file=outputFile)
+write.table(bioqcRes, file=outFile)
