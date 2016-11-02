88bb3d9cffd17105fef53bb70c27f4a350d04c27
Gregor Sturm <mail@gregor-sturm.de>
2016-11-02 17:24:58 +0100

    Update query to detect contamination. Update expected signature mapping.

--- db/queries/get_contamined_samples.sql
+++ db/queries/get_contamined_samples.sql
@@ -1,7 +1,12 @@
 with significant_samples as (
     select distinct gsm from bioqc_res where pvalue < 1e6
 ) -- all samples that have at least one score >6
-select * from bioqc_res r
+select 	r.*, 
+		bioqc_gsm.tissue, 
+		bts.signature as exp_sig, 
+		r2.pvalue as exp_sig_pval,
+		log(10, cast(r2.pvalue / r.pvalue as numeric)) as enrichment_ratio 
+from bioqc_res r
 join significant_samples ss on ss.gsm = r.gsm       -- fast filtering for siginifcant samples
 join bioqc_gsm on bioqc_gsm.gsm = r.gsm             -- get tissue annotation
 
@@ -12,11 +17,11 @@ join bioqc_tissues_signatures bts on bts.tissue = bioqc_gsm.tissue
 join bioqc_res r2 on r.gsm = r2.gsm and r.gse = r2.gse and r2.signature = bts.signature 
 
 -- this is the threshold by which the alternate signature must exceed the expected ones. 
-where r2.pvalue / r.pvalue > 1e5
+where (r2.pvalue / r.pvalue) > 1e8
 
 -- the expected signatures are likely to meet that threshold. Filter them out. 
 and r.signature not in (select signature from bioqc_tissues_signatures where tissue = bioqc_gsm.tissue) 
 
 -- choose the tissue.
-and bioqc_gsm.tissue = 'adipose'
+and bioqc_gsm.tissue = 'placenta'
 
